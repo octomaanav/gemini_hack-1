0bf7bebd841a0076e6b873f9eeb931fb819925c1
Manav Sharma <maanav.vashist@gmail.com>
2026-02-07 01:54:21 -0500

    feat: Conditionally set `sameSite` cookie policy to 'none' in production and 'lax' otherwise.

--- api/src/server.ts
+++ api/src/server.ts
@@ -71,7 +71,7 @@ app.use(
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     },
   })
 );
